7a3f9f24c75469e560c47803788483e463db53e7
Alif Danial <blinkixers@gmail.com>
2024-12-08 11:18:24 +0800

    New Variables

--- lab5a_q1.php
+++ lab5a_q1.php
@@ -5,18 +5,28 @@
 </head> 
 <body> 
     <?php  
-        $name = "Nur Ariffin Mohd Zin"; 
+        $name = "Muhammad Alif Danial Bin Zakaria"; 
         $matric = "AI220081";
         $course = "BIP";
         $year = 3;
         $address = "Kolej Kediaman Tun Dr. Ismail"
     ?> 
 
-    <table> 
-        <tr> 
-            <td>Name</td> 
-            <td><?php echo "$name"; ?></td>  
-        </tr> 
+    <table border="1"> 
+        <tr>
+            <th>Name</th>
+            <th>Matric Number</th>
+            <th>Course</th>
+            <th>Year Of Study</th>
+            <th>Address</th>
+        </tr>
+        <tr>
+            <td><?php echo $name; ?></td>
+            <td><?php echo $matric; ?></td>  
+            <td><?php echo $course; ?></td>  
+            <td><?php echo $year; ?></td>  
+            <td><?php echo $address; ?></td>    
+        </tr>
     </table> 
 </body> 
 </html> 
